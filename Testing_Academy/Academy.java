92fe816e403d2f4301cc96e1be3235fa62af7677
ShurikPurik <139052541+ShurikPurik@users.noreply.github.com>
2024-03-14 09:34:26 +0200

    Add files via upload

---
+++ Testing_Academy/Academy.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "The enormous room on the ground floor
